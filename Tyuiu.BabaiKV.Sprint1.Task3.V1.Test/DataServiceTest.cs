ad12ccb9ebaca7cd24329e3055cc231cba22050b
Tyuiu.BabaiKV@outlook.com <Tyuiu.BabaiKV@outlook.com>
2024-09-28 00:51:30 +0500

    Task3

---
+++ Tyuiu.BabaiKV.Sprint1.Task3.V1.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+using Tyuiu.BabaiKV.Sprint1.Task3.V1.Lib;
+namespace Tyuiu.BabaiKV.Sprint1.Task3.V1.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            double r = 1;
+            double h = 2;
+            var res = ds.CylinderVolume(r,h);
+            Assert.AreEqual(6.28,res);
+        }
+    }
+}
---
+++ Tyuiu.BabaiKV.Sprint1.Task3.V1/Program.cs
@@ -0,0 +1,23 @@
+﻿using Tyuiu.BabaiKV.Sprint1.Task3.V1.Lib;
+namespace Tyuiu.BabaiKV.Sprint1.Task3.V1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            double r = 1;
+            double h = 2;
+            Console.WriteLine("Радиус цилиндра r: " +r);
+            Console.WriteLine("Высота цилиндра h: " +h);
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("Объём цилиндра : " + ds.CylinderVolume(r,h));
+            Console.ReadLine();
+        }
+    }
+}
