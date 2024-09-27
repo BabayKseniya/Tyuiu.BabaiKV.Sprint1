c898f0718f084cc324fc6275c79734d5032ab726
Tyuiu.BabaiKV@outlook.com <Tyuiu.BabaiKV@outlook.com>
2024-09-27 23:39:24 +0500

    Task2

---
+++ Tyuiu.BabaiKV.Sprint1.Task2.V29.Test/DataServiceTest.cs
@@ -0,0 +1,16 @@
+using Tyuiu.BabaiKV.Sprint1.Task2.V29.Lib;
+namespace Tyuiu.BabaiKV.Sprint1.Task2.V29.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            int x = 120;
+            var res = ds.ConvertSecondsToHours(x);
+            Assert.AreEqual(2,res);
+        }
+    }
+}
---
+++ Tyuiu.BabaiKV.Sprint1.Task2.V29/Program.cs
@@ -0,0 +1,23 @@
+﻿using Tyuiu.BabaiKV.Sprint1.Task2.V29.Lib;
+namespace Tyuiu.BabaiKV.Sprint1.Task2.V29
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            int x;
+            Console.WriteLine("Время в секундах : "); 
+            x= Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("Время в минутах: " + ds.ConvertSecondsToHours(x));
+            Console.ReadLine();
+
+        }
+    }
+}
