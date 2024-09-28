bc034f6f9f724c798f3d79d60f4a5a05fd77d5d8
Tyuiu.BabaiKV@outlook.com <Tyuiu.BabaiKV@outlook.com>
2024-09-28 21:31:02 +0500

    Task4

---
+++ Tyuiu.BabaiKV.Sprint1.Task4.V13/Program.cs
@@ -0,0 +1,26 @@
+﻿using Tyuiu.BabaiKV.Sprint1.Task4.V13.Lib;
+namespace Tyuiu.BabaiKV.Sprint1.Task4.V13
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            int x, y;
+            Console.WriteLine("Введите значение x: ");
+            x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите значение y: ");
+            y = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("(cos(PI/x)/3*e^(x+y)) = " + ds.Calculate(x, y));
+            Console.ReadLine();
+            
+         
+        }
+    }
+}
